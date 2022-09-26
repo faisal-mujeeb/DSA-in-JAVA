e0bca83b067e9d10657d8b8aa640eff3ed388293
Faisal Mujeeb <107312668+faisal-mujeeb@users.noreply.github.com>
2022-09-26 14:23:33 +0530

    Add files via upload

---
+++ OrderAgnosticBS.java
@@ -0,0 +1,33 @@
+public class OrderAgnosticBS {
+    public static void main(String[] args) {
+        
+    }
+    static int OrderAgnosticBS(int arr[],int target){
+        int start = 0;
+        int end = arr.length-1;
+
+        boolean isAsc = arr[start] < arr[end];
+
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(arr[mid] == target){
+                return mid;
+            }
+            if (isAsc)
+            if(target<arr[mid]){
+                end = mid-1;
+            }else{
+                start = mid+1;
+            }
+        }else  {
+            int mid;
+            if(target>arr[mid]){
+                end = mid-1;
+            }else{
+                start = mid+1;
+            }
+
+        }
+        return -1;
+    }
+}
